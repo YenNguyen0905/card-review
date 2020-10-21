c325a309d163886d363ef1af03191f83b9afbae5
Yen Nguyen <yenn2k19@gmail.com>
2020-10-21 19:32:08 -0400

    Updates

---
+++ precache-manifest.d60a401ce4f80dd098f10b5cec7e18b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05047fa710140088b3ba4fd5b93d67b1",
+    "url": "/YenNguyen0905/card-review.git/index.html"
+  },
+  {
+    "revision": "5dae4e8722a59a212489",
+    "url": "/YenNguyen0905/card-review.git/static/css/main.e60d83a7.chunk.css"
+  },
+  {
+    "revision": "867122c0af8f61e4fa0b",
+    "url": "/YenNguyen0905/card-review.git/static/js/2.d53214b7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/YenNguyen0905/card-review.git/static/js/2.d53214b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dae4e8722a59a212489",
+    "url": "/YenNguyen0905/card-review.git/static/js/main.87dc63dc.chunk.js"
+  },
+  {
+    "revision": "f6a61ef6cf2feb8f1f12",
+    "url": "/YenNguyen0905/card-review.git/static/js/runtime-main.f028c33a.js"
+  }
+]);
